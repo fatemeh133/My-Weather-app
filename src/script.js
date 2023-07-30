808f7ae5cdaa93da3c3fa8367d2cfd521d713150
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2023-07-30 14:08:26 +0330

    import forcast data by API

--- src/script.js
+++ src/script.js
@@ -1,29 +1,48 @@
-function forcast() {
+function forcast(response) {
   let forcast = document.querySelector(".forcast");
-
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let html = `<div class="row">`;
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    html =
-      html +
-      `<div class="col-2 px-4">
-            <div class="forcast-date">${day}</div>
+  forecast.forEach(function (day, index) {
+    if (index < 5) {
+      html =
+        html +
+        `<div class="col-2 mx-2">
+            <div class="forcast-date">${formatDay(day.time)}</div>
             <div>
               <img
-                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
+                src="${day.condition.icon_url}"
                 alt=""
                 class="forcast-img"
               />
               <div class="forecast-temp">
-                <span class="forcast-max">18</span>°
-                <span class="forcast-min"> 12°</span>
+                <span class="forcast-max">${Math.round(
+                  day.temperature.maximum
+                )}</span>°
+                <span class="forcast-min"> ${Math.round(
+                  day.temperature.minimum
+                )}°</span>
               </div>
           </div>
         </div>`;
+    }
   });
 
   html = html + `</div>`;
   forcast.innerHTML = html;
+}
+
+function getForcast(city) {
+  let APIKey = "dfe65tb0afb4b600dd45b2f7o23f463d";
+  let APIURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${APIKey}`;
+  axios.get(APIURL).then(forcast);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
 }
 
 function formatDate(timestamp) {
@@ -71,6 +90,7 @@ function search(city) {
   let APIKey = "dfe65tb0afb4b600dd45b2f7o23f463d";
   let APIURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${APIKey}&units=metric`;
   axios.get(APIURL).then(Weather);
+  getForcast(city);
 }
 
 search("Tehran");
